4dc50c2d5f3415c594f644972371c702339b5bca
AotoFujita <d24114@numazu.kosen-ac.jp>
2026-02-18 11:08:20 +0900

    UPDATE Cleaning_Noticer.inp

--- Cleaning_Noticer.ino
+++ Cleaning_Noticer.ino
@@ -25,13 +25,13 @@
 // time to show a room number
 #define STIME 1500
 
-unsigned int timc = 0;                                                                                           // 500ms counter (0 ~ 499)
-unsigned int ntim = 0; // 1284;                                                                                           // current time(0 ~ 1439);
+unsigned int timc = 0;                                                                                          // 500ms counter (0 ~ 499)
+unsigned int ntim = 0; // 1284;                                                                                 // current time(0 ~ 1439);
 uint8_t floors[3][4] = {{ 102, 105, 107, 108 }, { 109, 113, 118, 111 }, { 112, 114, 115, 117 } };               // room number list Init(floor cleaning)
-uint8_t bath[3][5] = { { 106, 102, 104, 107, 108 }, { 103, 109, 113, 118, 111 }, { 105, 112, 114, 115, 117 } };  // room number list Init(bathroom cleaning)
+uint8_t bath[3][5] = { { 106, 102, 104, 107, 108 }, { 103, 109, 113, 118, 111 }, { 105, 112, 114, 115, 117 } }; // room number list Init(bathroom cleaning)
 
 uint8_t keyf = 1;   // key of change of room number(floor cleaning 1) (0~2)
-uint8_t keyfd =3;  // key of change of room number(floor cleaning 2) (0~3)
+uint8_t keyfd =3;   // key of change of room number(floor cleaning 2) (0~3)
 uint8_t keyb = 2;   // kee of change of room number(bathroom cleaning)(0~2)
 
 uint8_t floA[4] = {0/* 102, 105, 107, 108 */};
@@ -59,12 +59,15 @@ bool Block = 0;
 
 void setup() {
   // put your setup code here, to run once:
+
+  // set timer interrupt
   set(500, timer_count);
 
   for (int i = 2; i <= 16; i++) {
     pinMode(i, OUTPUT);
   }
 
+  // floA, floB, baT Init
   cfloor();
   cbath();
 
@@ -79,6 +82,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+  // notification floor cleaning
   if (week == 0 || week == 2 || week == 4) {
     digitalWrite(FLOON, HIGH);
     if (ntim == 1320 && !Flock) {
@@ -91,6 +95,8 @@ void loop() {
   } else {
     digitalWrite(FLOON, LOW);
   }
+
+  // notification bathroom cleaning
   if ((nineDays + 1) % 3 == 0) {
     digitalWrite(BATHN, HIGH);
     if (ntim == 1285 && !Block) {
@@ -104,7 +110,7 @@ void loop() {
     digitalWrite(BATHN, LOW);
   }
 
-
+  // check floor cleaning
   if (digitalRead(FLOOC) == 0) {
     dpo = 0;
     ch = 1;
@@ -129,6 +135,7 @@ void loop() {
     dpo = 1;
   }
 
+  // check bath cleaning
   if (digitalRead(BATHC) == 0) {
     dpo = 0;
     digitalWrite(SPEAK, LOW);
@@ -140,28 +147,38 @@ void loop() {
     dpo = 1;
   }
 
+  // show current time
   runseg(ntim, NTIME);
   seven_seg();
 }
 
 void timer_count() {
   timc++;
+  
+  // probably unused
   if (timc % 2 == 0) {
     callno = 0;
   }
+
+  // status 3rd DP Pin(for show current time)
   dp = 1 - dp;
+
+  // update current tie
   if (timc >= 120) {
 
     timc = 0;
     ntim++;
     if (ntim >= 1440) {
+
       week = (week + 1) % 7;
       nineDays = (nineDays + 1) % 9;
 
+      // update floA, floB
       if (week == 0) {
         cfloor();
       }
 
+      // update baT
       if ((nineDays + 1) % 3 == 1) {
         cbath();
       }
@@ -171,6 +188,8 @@ void timer_count() {
   }
 }
 
+
+// show the number for [fitme]msec
 void secshow(uint16_t ftime) {
 
   int ss = millis();
@@ -181,6 +200,7 @@ void secshow(uint16_t ftime) {
   }
 }
 
+// notification floor cleaning
 void tellF() {
   dpo = 0;
   ch = 1;
@@ -207,6 +227,7 @@ void tellF() {
   digitalWrite(SPEAK, LOW);
 }
 
+// notification bathroom cleaning
 void tellB() {
   dpo = 0;
   digitalWrite(SPEAK, HIGH);
@@ -221,6 +242,7 @@ void tellB() {
   dpo = 1;
 }
 
+// notification showing
 void loopshow(uint16_t ftime, uint8_t pin) {
   int ss = millis();
   int sn = ss;
